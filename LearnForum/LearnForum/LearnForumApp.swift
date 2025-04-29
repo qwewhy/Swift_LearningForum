cbb98968c70f29c5abae1dbaa70ca7b9e0568c77
WANG HONGYUAN <hongyuan.wang@student.uts.edu.au>
2025-04-29 15:52:17 +1000

    init

---
+++ LearnForum/LearnForum/LearnForumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnForumApp.swift
+//  LearnForum
+//
+//  Created by Hongyuan Wang on 29/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct LearnForumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearnForum/LearnForumTests/LearnForumTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnForumTests.swift
+//  LearnForumTests
+//
+//  Created by Hongyuan Wang on 29/4/2025.
+//
+
+import Testing
+@testable import LearnForum
+
+struct LearnForumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
