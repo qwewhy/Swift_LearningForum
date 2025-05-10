e5c45aa31949cab450c255acd6ea251c093704e2
Kyrie <2087543887@qq.com>
2025-05-11 01:25:35 +1000

    修改主页英文，添加用户中心
    - 主页修改为英文，错误检测的中文提示都没改！！！
    - ContentView样式改为用户中心，数据写死

---
+++ LearnForum/LearnForum/SettingsView.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsView.swift
+//  LearnForum
+//
+//  Created by 大鲸鱼 on 2025/5/11.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        VStack {
+            Text("Settings")
+                .font(.largeTitle)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SettingsView()
+}
